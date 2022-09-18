7fe1c45b56be72382e1371868f4d7eeb6069d462
帥　軍 <shuaijunqian0203@gmail.com>
2022-09-18 22:13:13 +0900

    Initial commit

---
+++ src/main/java/com/cicd/demo/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.cicd.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/message")
+    public String message(){
+        return "this is a message.";
+    }
+}
