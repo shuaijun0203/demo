762adb2e708f9a8a73837367f278e3438965492b
帥　軍 <shuaijunqian0203@gmail.com>
2022-09-19 19:13:55 +0900

    DynamoDBとrabbitMQ機能追加。

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
 #sudo java -jar demo-0.0.1-SNAPSHOT.jar
-sudo java -jar -Dserver.port=8080 \
+sudo java -jar -Dserver.port=8081 \
     *.jar > /dev/null 2> /dev/null < /dev/null &
