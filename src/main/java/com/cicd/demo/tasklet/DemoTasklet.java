3dcb9c74400b6252630c441b791af32674d84a61
帥　軍 <shuaijunqian0203@gmail.com>
2022-09-26 23:48:19 +0900

    NySqlテストソースを除外。

--- src/main/java/com/cicd/demo/tasklet/DemoTasklet.java
+++ src/main/java/com/cicd/demo/tasklet/DemoTasklet.java
@@ -21,29 +21,29 @@ public class DemoTasklet implements Tasklet {
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 
         // データベースに接続
-        Connection conn;
-
-        try {
-
-            conn = DriverManager.getConnection(
-                    "jdbc:mysql://database-demo.cjs3txxw4gba.ap-northeast-1.rds.amazonaws.com:3306/database-demo",
-                    "admin",
-                    "password"
-            );
-
-            try {
-                // SQL文の実行
-                System.out.println("JDBCデータベース接続成功");
-                System.out.println("conn = " + conn);
-                conn.close();
-            }
-            finally {
-                // 直前の try ブロックに入ったら、ここは必ず実行される
-                conn.close();
-            }
-        } catch (SQLException ex) {
-            // 接続できない時
-        }
+//        Connection conn;
+//
+//        try {
+//
+//            conn = DriverManager.getConnection(
+//                    "jdbc:mysql://database-1.cjs3txxw4gba.ap-northeast-1.rds.amazonaws.com:3306/database-1",
+//                    "admin",
+//                    "mypassword"
+//            );
+//
+//            try {
+//                // SQL文の実行
+//                System.out.println("JDBCデータベース接続成功");
+//                System.out.println("conn = " + conn);
+//                conn.close();
+//            }
+//            finally {
+//                // 直前の try ブロックに入ったら、ここは必ず実行される
+//                conn.close();
+//            }
+//        } catch (SQLException ex) {
+//            // 接続できない時
+//        }
         System.out.println("DemoTasklet!!!" + message);
         return RepeatStatus.FINISHED;
     }
