a13d420adcb4d2e19b4f3093640876162101b495
帥　軍 <shuaijunqian0203@gmail.com>
2022-09-26 22:26:29 +0900

    NySqlテストソースを追加。

--- src/main/java/com/cicd/demo/tasklet/DemoTasklet.java
+++ src/main/java/com/cicd/demo/tasklet/DemoTasklet.java
@@ -9,6 +9,8 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
+import java.sql.*;
+
 @NoArgsConstructor
 @AllArgsConstructor
 public class DemoTasklet implements Tasklet {
@@ -18,6 +20,30 @@ public class DemoTasklet implements Tasklet {
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 
+        // データベースに接続
+        Connection conn;
+
+        try {
+
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://database-demo.cjs3txxw4gba.ap-northeast-1.rds.amazonaws.com:3306/database-demo",
+                    "admin",
+                    "password"
+            );
+
+            try {
+                // SQL文の実行
+                System.out.println("JDBCデータベース接続成功");
+                System.out.println("conn = " + conn);
+                conn.close();
+            }
+            finally {
+                // 直前の try ブロックに入ったら、ここは必ず実行される
+                conn.close();
+            }
+        } catch (SQLException ex) {
+            // 接続できない時
+        }
         System.out.println("DemoTasklet!!!" + message);
         return RepeatStatus.FINISHED;
     }
